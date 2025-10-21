aea4c2824efcf52c93ad9bda3e8f3f6483bf825e
Diego Cardenas <diegosp778@gmail.com>
2025-10-21 11:18:48 -0500

    implemet aws

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Parcial segundo Tercio AREP
 ## Diego Cardenas 
+
+
+Se implementan los servicios basicos para el manejo de peticiones 
+
+
+http://localhost:8081/linearsearch?list=10,20,13,40,60&value=13
+
+![](/img/test-linearsearch.png)
+
+http://localhost:8081/binarysearch?list=10,20,13,40,60&value=13
+
+![](/img/test-binarysearch.png)
+
+
+Demostracion de despliegue de servicios 
+
+![](/img/despliegue-servicios.png)
+
+Y como eran dos servicios se adjunta la prueba de la ejecucion del otro servicio 
+
+![](/img/despliegue-servicios2.png)
--- proxy/src/main/java/com/arep/math/conntroller/ProxyController.java
+++ proxy/src/main/java/com/arep/math/conntroller/ProxyController.java
@@ -1,9 +1,12 @@
 package com.arep.math.controller;
 
+import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,27 +17,23 @@ import com.arep.math.service.ProxyService;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin("*")
 public class ProxyController{
 
-    @Autowired
-    private ProxyService proxyService;
-
-    @GetMapping("/binarysearch")
-    public ResponseEntity<Map<String, Object>> petition(
-        @RequestParam("list") String list,
-        @RequestParam("value") int value
-    ){
-        return ResponseEntity.ok(proxyService.redirectPetitions(list, value));
-    }
-
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private final AtomicLong counter = new AtomicLong();
     
-    @GetMapping("/linearsearch")
-    public ResponseEntity<Map<String, Object>> petition2(
-        @RequestParam("list") String list,
-        @RequestParam("value") int value
-    ){
-        return ResponseEntity.ok(proxyService.redirectPetitions(list, value));
+    @GetMapping("/proxy")
+    public String handleRequest(@RequestParam String path){
+        return null;
     }
 
+    public String redirect(String path) throws IOException{
 
+        String target = "";
+
+        //if(target.startsWith(path))
+
+        return null;
+    }
 }
